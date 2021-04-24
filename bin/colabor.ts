1efcc47fba5684b40545fb28be13ae412fe655f8
danilo.lima <danilo.lima@pixida.com.br>
2021-04-24 02:33:18 -0300

    frontend react init

--- bin/colabor.ts
+++ bin/colabor.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { ColaborStack } from '../lib/colabor-stack';
 
 const app = new cdk.App();
-new ColaborStack(app, 'ColaborStack');
+new ColaborStack(app, 'ColaborStack', {
+    env: {
+      account: '723542210814', // Replace with your account id
+      region: 'sa-east-1',
+    },
+});
--- lib/colabor-stack.ts
+++ lib/colabor-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
 
 export class ColaborStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new sns.Topic(this, 'MyTopic', {
+      displayName: 'MySuperTopic',
+    });
   }
 }
