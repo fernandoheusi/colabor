aba8b88f0c53af3ce7a5e8935a15208a59551e10
danilo.lima <danilo.lima@pixida.com.br>
2021-05-10 11:51:59 -0300

    footer

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import {FC} from 'react';
 import '../shared/css.css';
 import logo from '../../assets/logo.svg';
+import footerLogo from '../../assets/footer-logo.svg';
 import cart from '../../assets/cart.svg';
 import {Link, useLocation} from 'react-router-dom';
 
@@ -21,6 +22,7 @@ const Header = function () {
                 <Link to='/' className={`/${path.split('/')[1]}` === '/' ? selectedClass : notSelectedClass}>Loja</Link>
                 <Link to='/blog' className={`/${path.split('/')[1]}` === '/blog' ? selectedClass : notSelectedClass}>Blog</Link>
                 <Link to='/contato' className={`/${path.split('/')[1]}` === '/contato' ? selectedClass : notSelectedClass}>Contato</Link>
+                <Link to='/sobre' className={`/${path.split('/sobre')[1]}` === '/' ? selectedClass : notSelectedClass}>Sobre</Link>
                 <Link to='login' className={`/${path.split('/')[1]}` === '/login' ? selectedClass : notSelectedClass}>Cadastro/Login</Link>
             </div>
             <img className={'header-cart-img'} src={cart} alt={'carinho'}/>
@@ -28,28 +30,53 @@ const Header = function () {
     )
 }
 
-/*const Footer = function () {
+const Footer = function () {
+    const {pathname: path} = useLocation();
+    const selectedClass = 'active header-menu-item';
+    const notSelectedClass = 'header-menu-item';
     return (
         <div className={'footer-box'}>
             <div className={'footer-upper-subbox'}>
                 <div className={'footer-logo-img-wrapper'}>
                     <img className={'footer-logo-img'} src={footerLogo}/>
                 </div>
-                <div className={'footer-pague-com'}>Pague com:</div>
+                <div className={'footer-pague-com'}>
+                    Pague com:
+                    <br/>
+                    <img className={'footer-pague-com-img'} src={logo}/>
+                    <img className={'footer-pague-com-img'} src={logo}/>
+                    <img className={'footer-pague-com-img'} src={logo}/>
+                    <img className={'footer-pague-com-img'} src={logo}/>
+                </div>
+                <div className={'footer-login'}>
+                    Faça seu Login:<br/>
+                    <input type={'email'}/><br/>
+                    <input type={'password'}/><br/><br/>
+                    <span style={{float:'right'}}>OK</span>
+                    <span style={{fontSize:'0.7em', fontWeight:'normal'}}>
+                        Ainda não tenho cadastro<br/>
+                        Esqueci minha senha
+                    </span>
+                </div>
+                <div className={'footer-links'}>
+                    <Link to='/' className={`/${path.split('/')[1]}` === '/' ? selectedClass : notSelectedClass} style={{marginRight:'100px'}}>Loja</Link>
+                    <Link to='/sobre' className={`/${path.split('/sobre')[1]}` === '/' ? selectedClass : notSelectedClass}>Sobre</Link><br/>
+                    <Link to='/blog' className={`/${path.split('/')[1]}` === '/blog' ? selectedClass : notSelectedClass} style={{marginRight:'100px'}}>Blog</Link>
+                    <Link to='/contato' className={`/${path.split('/')[1]}` === '/contato' ? selectedClass : notSelectedClass}>Contato</Link>
+                </div>
             </div>
             <div className={'footer-break-line'}></div>
+            <span style={{fontSize:'0.9em'}}>Instagram | Youtube | Behance | Facebook</span>
         </div>
     )
-}*/
+}
 
 const Layout: FC<ILayoutProps> = function ({children}) {
 	return (
 		<div style={{minHeight:'100%'}}>
             <Header/>
 			<div className={'ch-content-filler'}>{children}</div>
-            {
-                //<Footer/>
-            }
+            <Footer/>
 		</div>
 	);
 };
--- frontend/src/components/loja/Loja.tsx
+++ frontend/src/components/loja/Loja.tsx
@@ -1,8 +1,94 @@
-const Loja = function () {
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
+interface ICarrouselImage {
+    src: string;
+    link: string;
+}
+const getCarrouselImages = () => {
+    return [
+        {
+            src: 'https://wallpaperaccess.com/full/124573.jpg',
+            link: '/'
+        },
+        {
+            src: 'https://hdwallpaperim.com/wp-content/uploads/2017/08/22/435641-ultra-wide-space.jpg',
+            link: '/'
+        },
+        {
+            src: 'https://wallpaperaccess.com/full/124573.jpg',
+            link: '/'
+        },
+        {
+            src: 'https://hdwallpaperim.com/wp-content/uploads/2017/08/22/435641-ultra-wide-space.jpg',
+            link: '/'
+        },
+        {
+            src: 'https://wallpaperaccess.com/full/124573.jpg',
+            link: '/'
+        },
+        {
+            src: 'https://hdwallpaperim.com/wp-content/uploads/2017/08/22/435641-ultra-wide-space.jpg',
+            link: '/'
+        },]
+}
+
+const CarrouselButtons = (props:{numDots: number, indexCurentImage: number, updateDotBtn: (n: number)=>void}) => {
+    const {numDots, indexCurentImage, updateDotBtn} = props;
+    let dots = [];
+
+    for(let i = 0; i < numDots; i++)
+        dots.push(<div key={'dot'+i} id={'dot'+i} className={ i === indexCurentImage ? 'carrousel-dot ativo' : 'carrousel-dot'}></div>);
+
     return (
-        <h1>
-            Loja
-        </h1>
+        <div className={'carrousel-dot-box'}>
+            <div className={'left-arrow'} onClick={()=>updateDotBtn(-1)}><span></span></div>
+            {dots.map(dot=>dot)}
+            <div className={'right-arrow'} onClick={()=>updateDotBtn(1)}><span></span></div>
+        </div>
+    )
+}
+
+
+const Carrousel = () => {
+    const updateDot = () => {
+        setIndexCurentImage((indexCurentImage+1)%carrouselImages.length);
+    }
+    const updateDotBtn = (n: number) => {
+        var index = (indexCurentImage + n) < 0 ? carrouselImages.length-1 : (indexCurentImage+n)%carrouselImages.length;
+        setIndexCurentImage(index);
+    }
+
+    const [carrouselImages, setCarrouselImages] = useState(getCarrouselImages());
+    const [indexCurentImage, setIndexCurentImage] = useState(0);
+    const [timeoutCarrousel] = useState<ReturnType<typeof setTimeout>[]>([]);
+
+    useEffect(()=>{
+        timeoutCarrousel.map(t => clearTimeout(t));
+        timeoutCarrousel.push(setTimeout(()=>updateDot(), 7000));
+    }, [indexCurentImage]);
+
+    return (
+        <div className={'carrousel-box'}>
+            <div className={'carrousel-inner-box'} style={{width: 'calc( 100% *' + carrouselImages.length + ' )', transform: 'translateX(-' + 100/carrouselImages.length*indexCurentImage + '%)'}}>
+                {Object.values(carrouselImages).map((image) =>
+                    <Link to={image.link}><img src={image.src} style={{width: 'calc( 100% /' + carrouselImages.length + ' )'}}/></Link>
+                )}
+            </div>
+            {carrouselImages.length > 0 ?
+                <CarrouselButtons numDots={carrouselImages.length} indexCurentImage={indexCurentImage} updateDotBtn={updateDotBtn}/> : null
+            }
+        </div>
+    );
+}
+
+
+const Loja = () => {
+    return (
+        <>
+            <Carrousel/>
+        </>
     )
 }
 
